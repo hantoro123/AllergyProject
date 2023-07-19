70bdfe3dae3c658ca54ab94f4e5f23732f9991db
SangJun Han <grw05177@naver.com>
2023-07-19 16:32:33 +0900

    class based view로 변환

--- allergyProject/board/views.py
+++ allergyProject/board/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView, ListView
+from board.models import Board
+
 
 # Create your views here.
-def board (request):
-    return render(request, 'board.html')
+# def board (request):
+#     return render(request, 'board.html')
 
-def boarddetail (request):
-    return render(request, 'boarddetail.html')
+class BoardView(ListView):
+    model = Board
+    queryset = Board.objects.order_by('id')
+    context_object_name = "board_list"
+
+# def DetailView (request):
+#     return render(request, 'boarddetail.html')
+
+class DetailView(TemplateView):
+    template_name = 'board/board_detail.html'
+
+
+class BoardCreateView(CreateView):
+    model = Board
+    fields = "__all__"
+    success_url = reverse_lazy("board:board_list")
