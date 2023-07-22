baec699ea29b0b17f67c5ee577da188404a4d4c9
SangJun Han <grw05177@naver.com>
2023-07-22 19:20:09 +0900

    User Model 생성 및 속성 부여

--- allergyProject/signapp/models.py
+++ allergyProject/signapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=10)
+    email = models.EmailField(max_length=30)
+    phone = models.CharField(max_length=15)
+    password = models.CharField(max_length=30)
