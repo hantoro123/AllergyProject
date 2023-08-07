cb3168d0be7ea3ea89aba1177cfdc2b3379d1b2d
KimDaehwan <etukdh6433@naver.com>
2023-08-07 14:31:35 +0900

    Test pro-mysql-remote.py

--- allergyProject/pro-mysql-remote.py
+++ allergyProject/pro-mysql-remote.py
@@ -1,4 +1,9 @@
 import pymysql
 
-conn = pymysql.connect(host='localhost',user='root',password='2017018023',db='allergydb',charset='utf8')
+conn = pymysql.connect(host='localhost',user='root',password='2017018023',charset='utf8')
+cursor = conn.cursor()
 
+# 쿼리문 작성 #
+
+conn.commit()
+conn.close()
