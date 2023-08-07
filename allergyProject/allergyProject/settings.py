057fd6dbc0b0b9f9213c9b9982e5b16451836114
KimDaehwan <etukdh6433@naver.com>
2023-08-07 14:16:34 +0900

    Simply connect mysql

--- allergyProject/allergyProject/settings.py
+++ allergyProject/allergyProject/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'allergyProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'AllergyDB',
-#         'USER': 'root',
-#         'PASSWORD': '2017018023',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'AllergyDB',
+        'USER': 'root',
+        'PASSWORD': '2017018023',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
