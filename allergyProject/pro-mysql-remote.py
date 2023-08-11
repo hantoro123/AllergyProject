87b74aa0f52e0b8c2f552bc49072125ad21ba0a2
KimDaehwan <etukdh6433@naver.com>
2023-08-11 10:05:37 +0900

    Success Insert Data Into searchapp_product table

--- allergyProject/pro-mysql-remote.py
+++ allergyProject/pro-mysql-remote.py
@@ -4,39 +4,19 @@ import requests
 import json
 import pymysql
 
-def Processed(dict, list):
+# dictionary인 prdlst를 tuple인 procData로 변경 #
+def Processed(dict):
 
-    procData = []
+    procData = (dict['prdlstReportNo'],
+                dict['prdlstNm'],
+                dict['prdkind'],
+                dict['rawmtrl'],
+                dict['allergy'],
+                dict['imgurl1'],
+                dict['manufacture'])
 
-        # if 'rnum' in list: procData.append(dict['rnum'])
-    # else: procData.append('')
-
-    if 'prdlstReportNo' in list: procData.append(dict['prdlstReportNo'].replace("\n", " "))
-    else: procData.append('')
-
-    if 'prdlstNm' in list: procData.append(dict['prdlstNm'].replace("\n", " "))
-    else: procData.append('')
-
-    # if 'productGb' in list: procData.append(dict['productGb'])
-    # else: procData.append('')
-
-    if 'prdkind' in list: procData.append(dict['prdkind'].replace("\n", " "))
-    else: procData.append('')
-
-    if 'rawmtrl' in list: procData.append(dict['rawmtrl'].replace("\n", ","))
-    else: procData.append('')
-
-    if 'allergy' in list: procData.append(dict['allergy'].replace("\n", ","))
-    else: procData.append('')
-
-    if 'imgurl1' in list: procData.append(dict['imgurl1'])
-    else: procData.append('')
-
-    if 'manufacture' in list: procData.append(dict['manufacture'].replace("\n", " "))
-    else: procData.append('')
-
-    # if 'imagurl2' in list: procData.append(dict['imagurl2'])
-    # else: procData.append('')
+    # 확인용 #
+    # print(procData)
 
     return procData
 
@@ -67,28 +47,25 @@ try:
         res = requests.get(URL, params=parameters, verify=False)
 
         print(res.url)
+        print(res.text)
 
         if res:
-            print(5)
-            data = json.loads(res.text)
+            data = json.loads(res.text)['body']['items']
 
-            body = data['body']
-            item_lst = body['items']
-
-            for i in item_lst:
+            for i in data:
                 prdlst = i['item']
-                field = prdlst.keys()
 
-                procData = Processed(prdlst, field)
+                field = prdlst.keys()
+                procData = Processed(prdlst)
 
                 # 수정 필요 #
-                cur.execute("INSERT INTO searchapp_product ('prdlstReportNo','prdlstNm','rawmtrl','allergy','manufacture','prdkind','image') VALUES (?,?,?,?,?,?,?)", procData)
+                # 사항 : data가 없으면 insert 있으면 update #
+                cur.execute("""INSERT INTO searchapp_product(prdlstReportNo, prdlstNm, prdkind, rawmtrl, allergy, image, manufacture) VALUES(%s, %s, %s, %s, %s, %s, %s)""", procData)
                 conn.commit()
             
             pageNo += 1
 
         else:
-            print(6)
             break
 finally:
     conn.close()
