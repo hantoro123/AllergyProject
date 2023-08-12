d3e24a5236f3a04590bf3a93308fdc88a18b1ab6
KimDaehwan <etukdh6433@naver.com>
2023-08-12 10:49:00 +0900

    File modification done.

--- allergyProject/searchapp/recommend-mysql.py
+++ allergyProject/searchapp/recommend-mysql.py
@@ -38,6 +38,40 @@ def top_match(data, allergy, index=3, sim_function=sim_person):
     return li[:index]
 
 
+# 서로 다른 알레르기 간의 유사도 #
+def getRecommendation (data, allergy, sim_function=sim_person):
+    result = top_match(proAlData, allergy, len(data))
+
+    simSum=0
+    score=0
+    li=[]
+    score_dic={}
+    sim_dic={}
+
+    for sim, al in result:
+        if sim < 0 : continue
+        for product in data[al].keys():
+            if data[al][product] == 0:
+                score += sim * data[allergy][product]
+                score_dic.setdefault(product,0)
+                score_dic[product] += score
+
+                sim_dic.setdefault(product, 0)
+                sim_dic[product] += sim
+            
+            score = 0
+
+    for key in score_dic:
+        score_dic[key] = score_dic[key]/sim_dic[key]
+        if score_dic[key] != 0:
+            li.append((score_dic[key],key))
+
+    li.sort()
+    li.reverse()
+
+    return li
+
+
 # DB 연결 #
 conn = pymysql.connect(host='localhost',
                        user='root',
@@ -47,20 +81,29 @@ conn = pymysql.connect(host='localhost',
 cur = conn.cursor()
 
 # Mysql에서 DATA 읽기 (전처리 포함) #
-cur.execute("""SELECT prdlstReportNo,prdlstNm FROM searchapp_product""")
+cur.execute("""SELECT prdlstReportNo FROM searchapp_product""")
 proData = cur.fetchall()
-print(proData[0])        # product data 확인용
+# print(proData[0:2])                                             # product data 확인용
 
-cur.execute("""SELECT gender,older,allergy,prdlstReportNo,prdlstNm,rating FROM searchapp_userdata""")
+cur.execute("""SELECT gender,older,allergy,prdlstReportNo,rating FROM searchapp_userdata""")
 choData = cur.fetchall()
-print(choData[0])         # user data 확인용
+# print(choData[0:2])                                             # user data 확인용
 
 # 형식 변환 #
 pdProData = pd.DataFrame(proData)
 pdChoData = pd.DataFrame(choData)
 
 # 병합 #
-merge_data = pd.concat([pdProData, pdChoData], join='inner')
-print(merge_data)
+merge_data = pd.concat([pdProData, pdChoData], join='outer')
+# print(merge_data)
+
+# 데이터 분포 #
+proAlData = merge_data.pivot_table(4, index=3, columns=2)       # 4 : 'rating', 3 : 'prdlstReportNo', 2: 'allergy'
+proAlData.fillna(0, inplace=True)                               # NaN -> 0
+print(proAlData)
+
+# 결과 #
+re = getRecommendation(proAlData, '난류')                       # userdata 수집 필요
+print(re[:10])
 
 conn.close()
