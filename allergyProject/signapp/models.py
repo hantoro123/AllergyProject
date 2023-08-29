07cd1944ba02671ecd4779fd0d42a5c189851b6f
Kimdaehwan <etukdh6433@naver.com>
2023-08-29 13:49:38 +0900

    Change Board_models and Signapp_models

--- allergyProject/signapp/models.py
+++ allergyProject/signapp/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    user_name = models.CharField(max_length=10)
-    email = models.EmailField(max_length=30)
-    phone = models.CharField(max_length=15)
-    password = models.CharField(max_length=30)
+class Customer(models.Model):
+    cno = models.CharField(max_length=8, primary_key=True)
+    username = models.CharField(max_length=8)
+    email = models.CharField(max_length=20)
+    phone = models.CharField(max_length=16)
+    bday = models.DateField()
+    gender = models.BooleanField()
+    pwd = models.CharField(max_length=20)
+    bookinfo = models.TextField(null=True)
+    boardinfo = models.TextField(null=True)
